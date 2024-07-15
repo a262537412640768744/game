61cc4e4d6d7045a85aa2e30305ba7c22a1b8b2da
unknown <358685701@qq.com>
2024-07-15 19:27:01 +0800

    first commit

---
+++ load.js
@@ -0,0 +1,44 @@
+function save() {
+    localStorage.setItem("homework", finishHomeworks.toString())
+    localStorage.setItem("teacher", teachers.toString())
+    localStorage.setItem("automate", machines.toString())
+    localStorage.setItem("max", maxHomework.toString())
+    localStorage.setItem("upgrades", JSON.stringify(upgrades))
+    localStorage.setItem("examinationPapers", examinationPapers.toString())
+    localStorage.setItem("EU", JSON.stringify(EU))
+    localStorage.setItem("auto", JSON.stringify(auto))
+    localStorage.setItem("finals", finalExaminations.toString())
+    localStorage.setItem("knowledge", knowledge.toString())
+    setTimeout(save, 0)
+}
+
+function get(name, default_, func) {
+    return localStorage.getItem(name) === null ? default_ : func(localStorage.getItem(name))
+}
+
+function load() {
+    finishHomeworks = get("homework", new D(0), n => new D(n))
+    teachers = get("teacher", new D(1), n => new D(n))
+    machines = get("automate", new D(0), n => new D(n))
+    maxHomework = get("max", new D(0), n => new D(n))
+    upgrades = get("upgrades", [false, false, false, false], JSON.parse)
+    examinationPapers = get("examinationPapers", new D(0), n => new D(n))
+    EU = get("EU", [false, false, false, false, false], JSON.parse)
+    auto = get("auto", false, JSON.parse)
+    finalExaminations = get("finals", new D(0), n => new D(n))
+    knowledge = get("knowledge", new D(0), n => new D(n))
+}
+
+function reset() {
+    choose = 0
+    finishHomeworks = new D(0)
+    teachers = new D(1)
+    machines = new D(0)
+    maxHomework = new D(0)
+    upgrades = [false, false, false, false]
+    examinationPapers = new D(0)
+    EU = [false, false, false, false, false]
+    auto = false
+    finalExaminations = new D(0)
+    knowledge = new D(0)
+}
